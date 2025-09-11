c3a20bac122815e73f9a1628688a2d7ae63cc2fc
matthew <itbeginshere@gmail.com>
2025-09-11 19:04:37 +0200

    Added rate limiting

--- ImgCompressor/Extensions/RateLimitingExtensions.cs
+++ ImgCompressor/Extensions/RateLimitingExtensions.cs
@@ -1,9 +1,42 @@
-﻿namespace MediaCompressor.API.Extensions;
+﻿using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
+
+namespace MediaCompressor.API.Extensions;
 
 internal static class RateLimitingExtensions
 {
     public static IServiceCollection AddPrjectRateLimiting(this IServiceCollection services)
     {
+        services.AddRateLimiter(options =>
+        {
+            options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+                RateLimitPartition.GetFixedWindowLimiter(
+                    partitionKey: httpContext.User.Identity?.Name ?? httpContext.Request.Headers.Host.ToString(),
+                    factory: partition => new FixedWindowRateLimiterOptions
+                    {
+                        AutoReplenishment = true,
+                        PermitLimit = 100,
+                        QueueLimit = 0,
+                        Window = TimeSpan.FromHours(1),
+                    }));
+
+            options.AddFixedWindowLimiter("fixed", opt =>
+            {
+                opt.PermitLimit = 10;
+                opt.Window = TimeSpan.FromSeconds(60);
+                opt.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+                opt.QueueLimit = 2;
+            });
+
+            options.OnRejected = async (context, cancellationToken) =>
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+                context.HttpContext.Response.Headers["Retry-AFter"] = "60";
+
+                await context.HttpContext.Response.WriteAsync("Rate limit exceeded. Please try again later.", cancellationToken);
+            };
+        });
+
         return services;
     }
 }
--- ImgCompressor/Program.cs
+++ ImgCompressor/Program.cs
@@ -22,6 +22,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
+app.UseRateLimiter();
+
 app.MapControllers();
 
 app.Run();
